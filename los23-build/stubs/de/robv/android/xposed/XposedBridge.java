a09c30318f22208055ff2adf2ce370731703014d
reDust Wildman <wizardwong9@gmail.com>
2026-08-25 17:21:02 -0700

    Add Xposed bridge stub

---
+++ los23-build/stubs/de/robv/android/xposed/XposedBridge.java
@@ -0,0 +1,8 @@
+package de.robv.android.xposed;
+import java.util.Set;
+public final class XposedBridge {
+    public static Set<XC_MethodHook.Unhook> hookAllConstructors(Class<?> hookClass, XC_MethodHook callback){ return null; }
+    public static Set<XC_MethodHook.Unhook> hookAllMethods(Class<?> hookClass, String methodName, XC_MethodHook callback){ return null; }
+    public static void log(String text){}
+    public static void log(Throwable t){}
+}
