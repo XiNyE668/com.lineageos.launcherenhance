e7f3414e41cbfc18f762a8a050acb8535a42cf83
reDust Wildman <wizardwong9@gmail.com>
2026-08-25 17:21:09 -0700

    Add Xposed helpers stub

---
+++ los23-build/stubs/de/robv/android/xposed/XposedHelpers.java
@@ -0,0 +1,9 @@
+package de.robv.android.xposed;
+public final class XposedHelpers {
+    public static Class<?> findClass(String className, ClassLoader classLoader){ return null; }
+    public static int getIntField(Object obj, String fieldName){ return 0; }
+    public static void setIntField(Object obj, String fieldName, int value){}
+    public static void setBooleanField(Object obj, String fieldName, boolean value){}
+    public static Object getObjectField(Object obj, String fieldName){ return null; }
+    public static Object callMethod(Object obj, String methodName, Object... args){ return null; }
+}
